f64b0c3819226418c6e959dab4de674166efd958
Fatih Tuzlu <fatihtuzlu07@gmail.com>
2024-12-14 13:33:39 +0300

    article revized according to searchable

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -3,8 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Article extends Model
 {
+    use Searchable;
+
     protected $fillable = ['title', 'content', 'category', 'source', 'published_at',];
+
+    public function toSearchableArray(): array
+    {
+        return [
+            'title' => $this->title,
+            'content' => $this->content,
+            'category' => $this->category,
+            'source' => $this->source,
+            'published_at' => $this->published_at,
+        ];
+    }
+
+    public function searchableAs(): string
+    {
+        return 'articles';
+    }
 }
